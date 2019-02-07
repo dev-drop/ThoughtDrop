45123920402b7006cd466df99f34ddb7182d608b
NomadicBea <32053542+NomadicBea@users.noreply.github.com>
2019-02-06 19:24:02 -0800

    Renamed login to index.php & main page to home.php

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ThoughtDrop</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+    <link rel="stylesheet" href="styles/main.css">
+
+
+</head>
+<body data-spy="scroll" data-target=".navbar-light" data-offset="300">
+
+
+
+<!--Navigation bar-->
+<nav id="navbar" class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" id="nav-brand" href="#">CompanyXYZ</a><br>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="nav-pills navbar-nav" id="navbar-navv">
+            <li class="nav-item">
+                <a class="nav-link" href="#skills">Search</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">Login</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<section class="bgimage">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="hero-text">
+                <h1>ThoughtDrop</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!---  Feed    ----------------------------------------------------------------->
+
+<div class="newsfeed container-fluid">
+  <div class="container">
+    <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Username</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+  </div>
+</div>
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -7,64 +7,50 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Raleway|Roboto" rel="stylesheet">
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
 
     <link rel="stylesheet" href="styles/main.css">
 
 
 </head>
-<body data-spy="scroll" data-target=".navbar-light" data-offset="300">
+<body class="loginpage">
 
+<div>
+  <div class="wrapper">
+    <div id="formContent">
+      <!-- Tabs Titles -->
 
-
-<!--Navigation bar-->
-<nav id="navbar" class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" id="nav-brand" href="#">CompanyXYZ</a><br>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul class="nav-pills navbar-nav" id="navbar-navv">
-            <li class="nav-item">
-                <a class="nav-link" href="#skills">Search</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#about">Login</a>
-            </li>
-        </ul>
-    </div>
-</nav>
-<section class="bgimage">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="hero-text">
-                <h1>ThoughtDrop</h1>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-<!---  Feed    ----------------------------------------------------------------->
-
-<div class="newsfeed container-fluid">
-  <div class="container">
-    <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Username</h5>
-          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+      <!-- Icon -->
+      <div class="fadeIn first">
+        <img src="images/cloud.png" id="icon" alt="ThoughtDrop" />
       </div>
+
+      <!-- Login Form -->
+      <form class="loginform" action="#" method="POST">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="password" placeholder="Password">
+        <input type="submit" value="Log In">
+      </form>
+
+
+      <!-- Registration Form -->
+      <form class="registrationform" action="#" method="POST">
+        <input type="text" name="id" placeholder="Enter Your Employee ID">
+        <input type="text" name="name" placeholder="Enter Your User Name">
+        <input type="text" name="password" placeholder="Enter Your Password">
+        <input type="submit" value="Register">
+      </form>
+
+      <!-- Footer with links -->
+      <div id="formFooter">
+        <div id="registerlink">New to ThoughtDrop? <a class="underlineHover" href="#">Register here</a></div>
+        <div id="loginlink">Already a User? <a class="underlineHover" href="#">Login here</a></div>
+      </div>
+    </div>
   </div>
 </div>
 
-
-
-
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="scripts/scripts.js"></script>
+<body>
 </html>
