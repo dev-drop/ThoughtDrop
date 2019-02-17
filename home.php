558b782a4faf6227fcf79092d1cdf94786baf307
NomadicBea <32053542+NomadicBea@users.noreply.github.com>
2019-02-17 15:33:18 -0800

    Home File styled - Enough

--- home.php
+++ home.php
@@ -1,3 +1,8 @@
+<?php
+require 'Includes/db.php'; require 'Includes/login.php';
+if (! empty($_SESSION['currentUser']))
+{
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +18,7 @@
 
 
 </head>
-<body data-spy="scroll" data-target=".navbar-light" data-offset="300">
+<body id="bod" data-spy="scroll" data-target=".navbar-light" data-offset="300">
 
 
 
@@ -25,17 +30,18 @@
     </button>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="nav-pills navbar-nav" id="navbar-navv">
+
             <li class="nav-item">
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
+              <div class="input-group mb-3 searchform">
+                <input id="searchbar" type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
                 <div class="input-group-append">
-                    <button class="btn btn-outline-light btn-sm" type="button" id="button-addon2"><img id="searchimg" src="images/search.png"/></button>
+                    <button class="btn btn-outline-light btn-sm searchbtn" type="button" id="button-addon2"><img id="searchimg" src="images/search.png"/></button>
                 </div>
               </div>
 
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#about">Login</a>
+                <a class="nav-link" href="/Semester5/ThoughtDrop%20Commits/ThoughDrop%20V1.2/Includes/logout.php">Logout</a>
             </li>
         </ul>
     </div>
@@ -57,32 +63,38 @@
 <div class="row">
   <div class="profile-container col">
     <div class="profile-body">
-      <div class="profile-img"><img src="images/LindainAdmin.jpg" alt="ProfileImg"></div>
+      <div class="profile-img"><img src="images/LindainAdmin.jpg"></div>
       <h4>Display Name<h4>
       <p>Department</p>
     </div>
   </div>
 
   <div class="col-8">
-
+    <div class="form-group">
+      <textarea placeholder="Drop Your Thoughts" class="form-control statusTA" id="exampleFormControlTextarea3" maxlength="300" onkeyup="auto_grow(this)" row="1"></textarea>
+      <div id="the-count">
+        <span id="current">0</span>
+        <span id="maximum">/ 300</span>
+      </div>
+    </div>
         <ul class="nav nav-tabs" id="myTab" role="tablist">
-          <li class="nav-item">
+          <li class="nav-item tab">
             <a class="nav-link active" id="all-tab" data-toggle="tab" href="#all" role="tab" aria-controls="all" aria-selected="true">All</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item tab">
             <a class="nav-link" id="rd-tab" data-toggle="tab" href="#rd" role="tab" aria-controls="rd" aria-selected="false">R&D</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item tab">
             <a class="nav-link" id="ms-tab" data-toggle="tab" href="#ms" role="tab" aria-controls="ms" aria-selected="false">M&S</a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item tab">
             <a class="nav-link" id="admin-tab" data-toggle="tab" href="#admin" role="tab" aria-controls="admin" aria-selected="false">Admin</a>
           </li>
         </ul>
         <div class="tab-content" id="myTabContent">
           <!--ALL TAB CONTENT----------------------------------------------->
           <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
-            <div class="card">
+            <div class="card all">
               <div class="card-body">
                 <h5 class="card-title">Username</h5>
                 <h6 class="card-subtitle mb-2 text-muted">I AM A USER FROM THE ALL TAB</h6>
@@ -90,10 +102,10 @@
               </div>
             </div>
           </div>
-          <!--ALL TAB CONTENT----------------------------------------------->
+          <!--RD TAB CONTENT----------------------------------------------->
           <div class="tab-pane fade" id="rd" role="tabpanel" aria-labelledby="rd-tab">
             <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
-              <div class="card">
+              <div class="card rd">
                 <div class="card-body">
                   <h5 class="card-title">Username</h5>
                   <h6 class="card-subtitle mb-2 text-muted">I AM A USER FROM THE RD TAB</h6>
@@ -106,7 +118,7 @@
           <!--ALL TAB CONTENT----------------------------------------------->
           <div class="tab-pane fade" id="ms" role="tabpanel" aria-labelledby="ms-tab">
             <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
-              <div class="card">
+              <div class="card ms">
                 <div class="card-body">
                   <h5 class="card-title">Username</h5>
                   <h6 class="card-subtitle mb-2 text-muted">I AM A USER FROM THE MS TAB</h6>
@@ -119,7 +131,7 @@
           <!--ALL TAB CONTENT----------------------------------------------->
           <div class="tab-pane fade" id="admin" role="tabpanel" aria-labelledby="admin-tab">
             <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
-              <div class="card">
+              <div class="card admin">
                 <div class="card-body">
                   <h5 class="card-title">Username</h5>
                   <h6 class="card-subtitle mb-2 text-muted">I AM A USER FROM THE ADMIN TAB</h6>
@@ -136,12 +148,14 @@
 </div>
 </div>
 
-
-
-
-
-
+<script src="scripts/scripts.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
+<?php
+}else
+{
+    echo 'You are not logged in. <a href="index.php">Click here</a> to log in.';
+}
+?>
